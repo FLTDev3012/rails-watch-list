1e88c7f14f668d60b362bd2e252b1702671f114d
dorian fiolet <dorian3012@hotmail.fr>
2023-02-24 12:49:33 +0100

    save

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -5,10 +5,7 @@ class BookmarksController < ApplicationController
   end
 
   def create
-    p params
-    p '---------------------'
     @list = List.find(params[:list_id])
-    p  @list
     @movie = Movie.find(params[:movie_id])
     @bookmark = Bookmark.new(list_id: @list.id, movie_id: @movie.id, comment: 'zobgfdgfd')
     @bookmark.save!
